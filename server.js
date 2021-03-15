21b4a3d0da1f8a45a51a4145793f8dc1f3f5f224
Senou Lynn <senou.lynn@gmail.com>
2021-03-15 16:56:32 -0400

    changing database

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.listen(port, () => {
 //USE THIS WHEN DEPLOYING
 const staticDir = path.resolve("./client/build");
 
-const AtlasURL = `mongodb+srv://${process.env.ATLASUSER}:${process.env.ATLASPASS}@cluster0.3sgwu.mongodb.net/test`
+// const AtlasURL = `mongodb+srv://${process.env.ATLASUSER}:${process.env.ATLASPASS}@cluster0.3sgwu.mongodb.net/test`
+const AtlasURL = `mongodb+srv://senoulynn:${process.env.ATLASPASS}@cluster0.3sgwu.mongodb.net/til?retryWrites=true&w=majority`
+
 
 app.use(express.static(staticDir));
 
