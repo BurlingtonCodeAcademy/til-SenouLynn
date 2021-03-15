c9aaaed9e128c9dc9261fe4f9eac0d1642f89fa6
Senou Lynn <senou.lynn@gmail.com>
2021-03-15 16:30:30 -0400

    heroku push

--- server.js
+++ server.js
@@ -12,12 +12,17 @@ app.listen(port, () => {
 
 //Static Directory
 const staticDir = path.resolve("./client/public");
+//USE THIS WHEN DEPLOYING
+// const staticDir = path.resolve("./client/build");
+const AtlasURL = `mongodb+srv://${process.env.ATLASUSER}:${process.env.ATLASPASS}@cluster0.3sgwu.mongodb.net/test`
+
 app.use(express.static(staticDir));
 
 //Bring in Mongoose dependencies
 const mongoose = require("mongoose");
 //Connect to specific database called 'til'
-mongoose.connect("mongodb://localhost:27017/til", {
+// "mongodb://localhost:27017/til"
+mongoose.connect(AtlasURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
